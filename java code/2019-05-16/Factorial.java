cfa96f38d2b8c574f3611f19dff7c5fb27c5188a
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-05-17 00:01:23 +0800

    5-16
    阶乘

--- java code/2019-05-16/Factorial.java
+++ java code/2019-05-16/Factorial.java
@@ -6,9 +6,21 @@ public class Factorial {
 			return factorial(n - 1) * n;
 	}
 	
+	public static long factorial2(int n) {
+		int count = 1;
+		if(n == 0)
+			return 1;
+		for(int i = 1; i <= n; i++) {
+			count *= i;
+		}
+		return count;
+	}
+	
 	public static void main(String[] args) {
 		int i = 6;
 		long n = factorial(i);
-		System.out.println(i + "的阶乘是：" + n);
+		System.out.println(i + " 的阶乘是：" + n);
+		long n2 = factorial2(i);
+		System.out.println(i + " 的阶乘是：" + n2);
 	}
 }
