e36739d960e47aa2f38b51a7f82c7ed1208afb36
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-05-23 22:56:52 +0800

    5-23

---
+++ java code/2019-05-23/Date.java
@@ -0,0 +1,22 @@
+/*
+*如何设计一个类
+*一.根据需求列出功能
+*1.传 年、月、日构造日期类
+*2.在当前日期上增加多少天
+*3.在当前日期上减少多少天
+*4.可以返回字符创 String 的方法		"2019-05-20"
+*5.加一些限制  年支持的范围[1900,2100]
+*6.如果给定两个日期，希望计算出下相差的天数
+*二.设计属性
+*1.年、月、日
+*/
+
+public class Date {
+	private int year;
+	private int month;
+	private int day;
+	
+	public static void main(String[] args) {
+		
+	}
+}
