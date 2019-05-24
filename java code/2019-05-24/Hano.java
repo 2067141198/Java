c285f3057289be3af79113409a51518925aa6be0
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-05-24 23:42:32 +0800

    5-24

--- java code/2019-05-24/Hano.java
+++ java code/2019-05-24/Hano.java
@@ -1,12 +1,15 @@
-public class Hanoi {
-	void HanioTest(int x) {
+public class Hano {
+	static void HanioTest(int x, char a, char b, char c) {
 		if(x == 1) {
-			System.out.println();
+			System.out.println(x + "从" + a + "移动到" + c);
+		}else{
+			HanioTest(x - 1, a, c, b);
+			System.out.println(x + "从" + a + "移动到" + c);
+			HanioTest(x - 1, b, a, c);
 		}
-			
 	}
 	
 	public static void main(String[] args) {
-		
+		HanioTest(6, 'a', 'b', 'c');
 	}
 }
