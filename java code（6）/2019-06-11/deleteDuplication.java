57311dee12d84c9a2b0bd344a0d105ef53b3d429
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-06-12 00:12:36 +0800

    Update deleteDuplication.java

--- java code（6）/2019-06-11/deleteDuplication.java
+++ java code（6）/2019-06-11/deleteDuplication.java
@@ -1,5 +1,4 @@
-/*
- public class ListNode {
+class ListNode {
     int val;
     ListNode next = null;
 
@@ -7,30 +6,32 @@
         this.val = val;
     }
 }
-*/
-public class Solution {
-    public ListNode deleteDuplication(ListNode pHead)
-    {
+
+public class DeleteDuplication {
+    public ListNode deleteDuplication(ListNode pHead) {
         ListNode prve = null;
-        ListNode p1 = pHead;
-        ListNode p2 = pHead.next;
-        if(p1.val != p2.val) {
-            prve = p1;
-            p1 = p1.next;
-            p2 = p2.next;
-        } else {
-            
-            if(p2.val != null && p2.val == p1.val) {
-                p2 = p2.next;
-            }
-            if(prve == null) {
-                pHead = p2; 
-            } else {
-                prve.next = p2;
-            }
-            p1 = p2;
-            p2 = p2.next;
-        }
-        return p1;
+		ListNode p1 = pHead;
+		ListNode p2 = pHead.next;
+		while(p2 != null) {
+			if(p1.val != p2.val) {
+				prve = p1;
+				p1 = p1.next;
+				p2 = p2.next;
+			} else {
+				while(p2 != null && p2.val == p1.val) {
+					p2 = p2.next;
+				}
+				if(prve == null) {
+					pHead = p2;
+				} else {
+					prve.next = p2;
+				}
+				p1 = p2;
+				if(p2 != null) {
+					p2 = p2.next;
+				}
+			}
+		}
+		return pHead;
     }
 }
