f307249864545055dbc476f09879e39df82b9347
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-06-18 23:48:16 +0800

    Create DLinkedList.java

---
+++ java code（6）/2019-06-18/DLinkedList.java
@@ -0,0 +1,24 @@
+
+class DNode {
+	int val;
+	Node next;
+	Node prev;
+	
+	Node(int val) {
+		this.val = val;
+	}
+}
+
+public class DLinkedList{
+	private DNode head;
+	private DNode last;
+	private int size;
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public void pushFront(int val) {
+		
+	}
+}
