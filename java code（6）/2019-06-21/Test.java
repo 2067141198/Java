9b7fcc4f2f9ea28618fdb0a2263e85b5c19a45eb
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-06-21 21:50:16 +0800

    Create Test.java
    代码块

---
+++ java code（6）/2019-06-21/Test.java
@@ -0,0 +1,38 @@
+class HelloA {
+	//构造⽅法
+	public HelloA(){
+		System.out.println("Hello A!父类构造方法");
+	}
+	//⾮静态代码块
+	{
+		System.out.println("i'm A class.父类非静态代码块");
+	}
+	//静态代码块
+	static{
+		System.out.println("static A 父类静态代码块");
+	}
+}
+class HelloB extends HelloA {
+	//构造⽅法
+	public HelloB(){
+		System.out.println("Hello B! 构造方法");
+	}
+	//⾮静态代码块
+	{
+		System.out.println("i'm B class.非静态代码块");
+	}
+	//静态代码块
+	static{
+		System.out.println("static B 静态代码块");
+	}
+} 
+public class Test{
+	public static void main(String[] args) {
+		 System.out.println("---start---");
+		 new HelloB();
+		 new HelloB();
+		 new HelloB();
+		 System.out.println("---end---");
+	}
+}
+ 
