9a290c8bfaeadaf24a766e23971782e33300b9c9
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-06-26 08:05:42 +0800

    Update ListInnerClass4.java

--- java code（6）/2019-06-25/ListInnerClass4.java
+++ java code（6）/2019-06-25/ListInnerClass4.java
@@ -11,4 +11,46 @@ interface list{
 	void set(int index, int val);
 	
 	int size(); 
+	
+	Iterator iterator();
 }
+
+interface Iterator {
+	boolean hasNext();
+	int next();
+}
+
+abstract class abstractList implements List {
+	protected int size;
+}
+
+class ArrayList extends abstractList implements List, Iterator {
+	private class ArrayListIterator implements Iterator {
+		
+	}
+	
+	
+}
+
+class LinkedList extends abstractList implements List, Iterator {
+	private class LinkedListIterator implements Iterator {
+		
+	}
+	
+	
+}
+
+public class ListInnerClass4 {
+	static void testList(List list) {
+		
+	}
+	
+	public static void main(String[] args) {
+		testList(new ArrayList());
+		testList(new LinkedList());
+	}
+}
+
+
+
+
