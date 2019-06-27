b1eab06979c5546f6cc056505fe6f1573b4038c4
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-06-27 20:54:42 +0800

    Update ListInnerClass4.java

--- java code（6）/2019-06-25/ListInnerClass4.java
+++ java code（6）/2019-06-25/ListInnerClass4.java
@@ -22,6 +22,44 @@ interface Iterator {
 
 abstract class abstractList implements List {
 	protected int size;
+	
+	public void pushFront(int val) {
+		insert(0, val);
+	}
+	
+	public void pushBack(int val) {
+		insert(size, val);
+	}
+	
+	public void insert(int index, int val) {
+		if(index < 0 || index > size) {
+			System.out.println("下标错误！");
+			return;
+		}
+		
+		insertInternal(index, val);
+	}
+	
+	abstract insertInternal(int index, int val);
+	
+	public void popFront() {
+		earse(0);
+	}
+	
+	public void popBack() {
+		earse(size);
+	}
+	
+	public void earse(int index) {
+		if(index < 0 || index > size) {
+			System.out.println("下标错误！");
+			return;
+		}
+		
+		earseInternal(index);
+	}
+	
+	abstract void earseInternal(int index);
 }
 
 class ArrayList extends abstractList implements List, Iterator {
