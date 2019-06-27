2b704e5680989523e4d4df9478af8f5c6c64f6e3
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-06-27 21:23:43 +0800

    Update ListInnerClass4.java

--- java code（6）/2019-06-25/ListInnerClass4.java
+++ java code（6）/2019-06-25/ListInnerClass4.java
@@ -1,4 +1,4 @@
-interface list{
+interface List{
 	void pushFront(int val);
 	void pushBack(int val);
 	void insert(int index, int val);
@@ -23,14 +23,17 @@ interface Iterator {
 abstract class abstractList implements List {
 	protected int size;
 	
+	@Override
 	public void pushFront(int val) {
 		insert(0, val);
 	}
 	
+	@Override
 	public void pushBack(int val) {
 		insert(size, val);
 	}
 	
+	@Override
 	public void insert(int index, int val) {
 		if(index < 0 || index > size) {
 			System.out.println("下标错误！");
@@ -40,16 +43,19 @@ abstract class abstractList implements List {
 		insertInternal(index, val);
 	}
 	
-	abstract insertInternal(int index, int val);
+	abstract void insertInternal(int index, int val);
 	
+	@Override
 	public void popFront() {
 		earse(0);
 	}
 	
+	@Override
 	public void popBack() {
 		earse(size);
 	}
 	
+	@Override
 	public void earse(int index) {
 		if(index < 0 || index > size) {
 			System.out.println("下标错误！");
@@ -60,32 +66,100 @@ abstract class abstractList implements List {
 	}
 	
 	abstract void earseInternal(int index);
+	
+	@Override
+	public int size() {
+		return size;
+	}
 }
 
-class ArrayList extends abstractList implements List, Iterator {
+class ArrayList extends abstractList implements List {
 	private class ArrayListIterator implements Iterator {
+		private int currentIndex;
 		
+		ArrayListIterator() {
+			this.currentIndex = 0;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return currentIndex < size();
+		}
+		
+		@Override
+		public int next() {
+			int val = array[currentIndex];
+			currentIndex++;
+			return val;
+		}
 	}
 	
+	private int[] array = new int[100];
 	
+	@Override
+	public void insertInternal(int index, int val) {
+		for(int i = size; i > index; i--) {
+			array[i] = array[i - 1];
+		}
+		array[index] = val;
+		size++;
+	}
+	
+	@Override
+	public void earseInternal(int index) {
+		for(int i = index; i < size; i++) {
+			array[i] = array[i + 1];
+		}
+		size--;
+	}
+	
+	@Override
+	public int get(int index) {
+		return array[index];
+	}
+
+	@Override	
+	public void set(int index, int val) {
+		array[index] = val;
+	}
+	
+	@Override
+	public Iterator iterator() {
+		return new ArrayListIterator();
+	}
 }
 
-class LinkedList extends abstractList implements List, Iterator {
-	private class LinkedListIterator implements Iterator {
+// class LinkedList extends abstractList implements List, Iterator {
+	// private class LinkedListIterator implements Iterator {
 		
-	}
+	// }
 	
 	
-}
+// }
 
 public class ListInnerClass4 {
 	static void testList(List list) {
+		list.pushFront(1);
+		list.pushFront(2);
+		list.pushFront(3);
+		list.pushFront(4);
+		list.pushFront(5);
+		list.pushBack(10);
+		list.pushBack(20);
+		list.pushBack(30);
+		list.pushBack(40);
+		list.pushBack(50);
 		
+		Iterator it1 = list.iterator();
+		
+		while(it1.hasNext()) {
+			System.out.println(it1.next());
+		}
 	}
 	
 	public static void main(String[] args) {
 		testList(new ArrayList());
-		testList(new LinkedList());
+		//testList(new LinkedList());
 	}
 }
 
