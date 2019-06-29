5cbbb47de19189360b5da62429c77e0176c8dc7e
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-06-29 10:16:55 +0800

    Create OddEvenList.java

---
+++ java code（6）/2019-06-29/OddEvenList.java
@@ -0,0 +1,42 @@
+
+Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        ListNode p1 = head;
+        ListNode p2 = head.next;
+        ListNode head2 = p2;
+        int nodes = 0;
+        for(ListNode cur = head; cur != null; cur = cur.next) {
+            nodes++;
+        }
+        int count = nodes % 2 - 1;
+        while(count > 0) {
+            p1.next = p2.next;
+            p1 = p1.next;
+            p2.next = p1.next;
+            p2 = p2.next;
+            count--;
+        }
+        if(nodes % 2 == 0) {
+            p1.next = p2.next;
+            p1 = p1.next;
+            p2.next = p1.next;
+            p2 = p2.next;
+            
+            p1.next = head2;
+        } else {
+            p1.next = p2.next;
+            p1 = p1.next;
+            
+            p1.next = head2;
+        }
+        
+        return head;
+    }
+}
