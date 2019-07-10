313bf8a24a09a96f0c000db63ce739971db9d11d
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-07-11 00:00:04 +0800

    Update Random2.java

--- java code（7）/2019-07-10/Random2.java
+++ java code（7）/2019-07-10/Random2.java
@@ -5,7 +5,8 @@ public class Random2 {
 		Random r = new Random();
 		
 		for(int i = 0; i < 100; i++) {
-			int num = r.nextInt(10);
+			int num = r.nextInt(10); //随机生成数字的范围实际是0~9
+			System.out.println(num);
 		}
 	}
 }
