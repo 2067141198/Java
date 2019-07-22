eb92872c05181089d0c618059f654e34b1874fb2
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-07-22 23:36:58 +0800

    输入一个整数，输出该整数转为二进制后1的个数， 修正输入为负数时无法正常输出二进制中1的个数。 添加部分注释

--- java code（7）/2019-07-22/Dome01/src/Dome01NumBinary.java
+++ java code（7）/2019-07-22/Dome01/src/Dome01NumBinary.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+/*
+* 输入一个数字，输出给数字在内存中二进制1的个数，正数负数都可以计算*/
 public class Dome01NumBinary {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,6 +15,13 @@ public class Dome01NumBinary {
                 num /= 2;
             }
         } else {
+            /*
+            * 先将负数转换为二进制互为补码的正整数；
+            * 负数与其对应正数小1的数的二进制互为补码
+            * 计算出补码中1的个数
+            * 便是负数中0的个数
+            * int为8字节共32位
+            * 减去0的个数便是负数二进制中1的个数*/
             int num2 = (-num) - 1;
             while(num2 != 0) {
                 if(num2 % 2 == 1) {
@@ -20,7 +29,7 @@ public class Dome01NumBinary {
                 }
                 num2 /= 2;
             }
-            tmp += 64;
+            tmp += 32;
             //System.out.println(num2);
         }
         System.out.println(num + "的二進制中1的个数是：" + tmp);
