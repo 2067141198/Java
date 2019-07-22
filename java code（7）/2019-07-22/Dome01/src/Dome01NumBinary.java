ab52c55c981e2b831636e69d6ffcf34aaa657cc6
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-07-22 23:14:48 +0800

    输入一个整数，输出该帧数转为二进制后1的个数

---
+++ java code（7）/2019-07-22/Dome01/src/Dome01NumBinary.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Dome01NumBinary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int tmp = 0;
+        if(num >= 0) {
+            while (num != 0) {
+                if (num % 2 == 1) {
+                    tmp++;
+                }
+                num /= 2;
+            }
+        } else {
+            int num2 = ~num;
+            while(num2 != 0) {
+                if(num2 % 2 == 1) {
+                    tmp++;
+                }
+                num2 /= 2;
+            }
+            System.out.println(num2);
+        }
+        System.out.println(num + "的二進制中1的个数是：" + tmp);
+    }
+}
