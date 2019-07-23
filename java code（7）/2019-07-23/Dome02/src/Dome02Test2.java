91498cf1f3fade92ccc186cb3d52f0927f16b161
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-07-23 11:31:50 +0800

    测试输出的结果

---
+++ java code（7）/2019-07-23/Dome02/src/Dome02Test2.java
@@ -0,0 +1,13 @@
+public class Dome02Test2 {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i < 10) {
+//            if(i < 1)
+//                continue;
+            if(i ==5)
+                break;
+            i++;
+        }
+        System.out.println(i);
+    }
+}
