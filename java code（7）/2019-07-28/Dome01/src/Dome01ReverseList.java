c11cb9147e5d6b1cfee459f3e4cb0c3ef9ea0faa
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-07-28 22:51:03 +0800

    通过输入的元素位置将数组分成前后两个部分，并打印

--- java code（7）/2019-07-28/Dome01/src/Dome01ReverseList.java
+++ java code（7）/2019-07-28/Dome01/src/Dome01ReverseList.java
@@ -13,16 +13,17 @@ public class Dome01ReverseList {
         System.out.print("请输入需要逆置的位置：");
         int k = sc.nextInt();
         //System.out.println(k);
-        char[] listFront = new char[list.length];
-        char[] listBack = new char[list.length];
-
-        for (int i = 0; i < k; i++) {
-            listFront[i] = list[i];
-        }
+        char[] listFront = new char[k];
+        char[] listBack = new char[list.length - k];
+        int lf = 0;
+        int lb = 0;
 
         for (int i = 0; i < list.length; i++) {
-            listBack[i] = list[k];
-            k++;
+            if(i <  k) {
+                listFront[lf++] = list[i];
+            } else {
+                listBack[lb++] = list[i];
+            }
         }
 
         System.out.println(Arrays.toString(listFront));
