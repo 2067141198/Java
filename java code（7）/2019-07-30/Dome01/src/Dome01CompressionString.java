1cfa5abdd86e084c05041b122c68d1ad2a8eca48
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-07-30 20:53:24 +0800

    将字符串保存成字符数组形式，然后将字符数组中重复的元素删除 将字符创压缩成元素加出现次数的格式并打印

--- java code（7）/2019-07-30/Dome01/src/Dome01CompressionString.java
+++ java code（7）/2019-07-30/Dome01/src/Dome01CompressionString.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Dome01CompressionString {
     public static void main(String[] args) {
         String s = "aabbaaccddeebf";
         char[] ch = s.toCharArray();
+        System.out.println("s = " + s);
         System.out.println(ch);
 
         //char[] compressionCh = new char[ch.length];
@@ -14,6 +16,7 @@ public class Dome01CompressionString {
 
         System.out.println(compressionCh);
 
+        //删除compressionCh中重复的元素
         for (int i = 0; i < compressionCh.size(); i++) {
             for (int j = i + 1; j < compressionCh.size(); j++) {
                 if(compressionCh.get(i) == compressionCh.get(j)) {
@@ -22,5 +25,25 @@ public class Dome01CompressionString {
             }
         }
         System.out.println(compressionCh);
+
+        //创建一个数组，用来储存compressionCh中对应位置元素出现的次数
+        int[] appearNum = new int[compressionCh.size()];
+        for (int i = 0; i < compressionCh.size(); i++) {
+            for (int j = 0; j < ch.length; j++) {
+                if(compressionCh.get(i) == ch[j]) {
+                    appearNum[i]++;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(appearNum));
+
+        //最终压缩后的字符串
+        ArrayList<Character> newCompressionCh  = new ArrayList<>();
+        for (int i = 0; i < appearNum.length; i++) {
+            newCompressionCh.add(compressionCh.get(i));
+            newCompressionCh.add((char)(appearNum[i] + 48));
+        }
+        System.out.println(newCompressionCh);
     }
 }
