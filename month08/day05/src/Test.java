66ab3a350c1e7065220c08ad322073b52381b323
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-08-05 23:44:47 +0800

    判断一个数是不是2的整数次方

--- month08/day05/src/Test.java
+++ month08/day05/src/Test.java
@@ -4,17 +4,28 @@ public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i = sc.nextInt();
-        int tmp  = i;
-        int count = 0;
-        while(i > 1) {
-            i /= 2;
-            count++;
+        int tmp  = i; //定义一个tmp用来保存i的值
+        int count = 0;//用来计输入的数为2的次方幂
+
+        //先通过取模判断输入的数是否是2的整数次方
+        while(i >= 2) {
+            i %= 2;
         }
 
-        if(i % 2 == 0) {
-            System.out.println("输入的数字" + tmp + "是2的" + count + "次方");
-        } else {
+        if(i % 2 != 0) {
             System.out.println("该数不是2的整数次方");
+        } else {
+
+            //来计算数字i的次方幂
+            //因为1是2的0次方，所以只能以输入的数字i大于等于2时来作为循环条件
+            while(tmp >= 2) {
+                count++;
+                tmp /= 2;
+            }
+            System.out.println("输入的数字是2的" + count + "次方");
         }
     }
 }
+
+
+
