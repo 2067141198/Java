69b698f4bf153fe041c7bedc159f0ba91ab469c3
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-08-14 21:15:12 +0800

    使用Runtime类中exec() 方法通过运行notepad.exe 打开一个Windows自带的记事本程序，一定时间后关闭

---
+++ month08/day14/src/day14TestExec.java
@@ -0,0 +1,9 @@
+
+public class day14TestExec {
+    public static void main(String[] args) throws Exception {
+        Runtime rt = Runtime.getRuntime();          //创建一个Runtime实例对象
+        Process process = rt.exec("notepad.exe");       //得到表示进程的Process对象
+        Thread.sleep(3000);             //程序休眠3秒
+        process.destroy();                     //关闭程序
+    }
+}
