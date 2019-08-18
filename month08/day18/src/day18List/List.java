9ef47771e1b853ad8432d464522fc76080aeb85e
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-08-18 13:10:41 +0800

    顺序表接口

---
+++ month08/day18/src/day18List/List.java
@@ -0,0 +1,17 @@
+package day18List;
+
+public interface List {
+    boolean add(int element);
+
+    boolean add(int index, int element);
+
+    int get(int index);
+
+    int set(int index, int element);
+
+    boolean remove(int index);
+
+    int size();
+
+    boolean isEmpty();
+}
