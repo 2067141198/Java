9782a7a2de70f9365ed9c4d8a7ed765d7ae79dd4
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-08-21 22:58:11 +0800

    在数组中选三个数字作为三角形的边长，在满足可以围成三角形的前提下，返回周长最长额三角形的周长

---
+++ month08/day21/src/LargestPerimeter.java
@@ -0,0 +1,19 @@
+public class LargestPerimeter {
+    public int largestPerimeter(int[] A) {
+        int maxLargest = 0;
+        int largest = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = i + 1; j < A.length; j++) {
+                for (int z = j + 1; z < A.length; z++) {
+                    if(A[i] + A[j] > A[z] || A[i] + A[z] > A[j] || A[j] + A[z] > A[i]) {
+                        largest = A[i] + A[j] + A[z];
+                    }
+                    if(largest > maxLargest)
+                        maxLargest = largest;
+                }
+            }
+        }
+        return maxLargest;
+    }
+}
