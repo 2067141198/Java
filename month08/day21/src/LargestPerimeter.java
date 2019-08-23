435958c59621e0915a1763b458730c87dd9e744d
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-08-23 18:30:15 +0800

    三角形的最大周长

--- month08/day21/src/LargestPerimeter.java
+++ month08/day21/src/LargestPerimeter.java
@@ -1,12 +1,15 @@
 public class LargestPerimeter {
     public int largestPerimeter(int[] A) {
+
         int maxLargest = 0;
         int largest = 0;
 
         for (int i = 0; i < A.length; i++) {
             for (int j = i + 1; j < A.length; j++) {
                 for (int z = j + 1; z < A.length; z++) {
-                    if(A[i] + A[j] > A[z] || A[i] + A[z] > A[j] || A[j] + A[z] > A[i]) {
+                    if(A[i] + A[j] > A[z] && A[i] < A[z] && A[j] < A[z]
+                            || A[i] + A[z] > A[j] && A[i] < A[j] && A[z] < A[j]
+                            || A[j] + A[z] > A[i] && A[j] < A[i] && A[z] < A[i]) {
                         largest = A[i] + A[j] + A[z];
                     }
                     if(largest > maxLargest)
