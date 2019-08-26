251ac038afc8699c584223aa2ecca363e81880e7
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-08-26 22:54:22 +0800

    层序遍历

--- month08/day26/src/LevelOrder.java
+++ month08/day26/src/LevelOrder.java
@@ -1,7 +1,11 @@
 import java.util.List;
 import java.util.*;
 
+/**
+ * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+ */
 public class LevelOrder {
+    //定义一个内部类，使其具有两个属性，一个表示树的结点，一个表示到达的层数
     static class Element {
         TreeNode root;
         int level;
@@ -17,10 +21,13 @@ public class LevelOrder {
             return list;
         }
         Queue<Element> queue = new LinkedList<>();
+        //将包含头结点的自定义类的对象添加入队列
         queue.add(new Element(root, 0));
 
         while(queue.peek() != null) {
+            //取出队列中的第一个元素
             Element front = queue.remove();
+            //当list的元素个数等于遍历到达的层数时说明list中没有对象，需要添加一个新的ArrayList对象
             if(list.size() == front.level) {
                 list.add(new ArrayList<>());
             }
