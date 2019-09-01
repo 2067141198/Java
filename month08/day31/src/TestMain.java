ff6d0837d594233bcaf9795414689c28c4e0c21a
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-09-01 09:37:38 +0800

    测试归并排序递归和非递归

---
+++ month08/day31/src/TestMain.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class TestMain {
+    public static void main(String[] args) {
+        int[] array = new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int[] array2 = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] array3 = new int[]{1, 3, 4, 6, 8, 9, 0, 5, 2, 7};
+        System.out.println("array数组为：" + Arrays.toString(array));
+        System.out.println("array2数组为：" + Arrays.toString(array2));
+        System.out.println("array3数组为：" + Arrays.toString(array3));
+        MergeSort.mergeSort(array);
+        MergeSort.mergeSort(array2);
+        MergeSort.mergeSort(array3);
+        System.out.println("array数组归并排序后结果是：" + Arrays.toString(array));
+        System.out.println("array2数组归并排序后结果是：" + Arrays.toString(array2));
+        System.out.println("array3数组归并排序后结果是：" + Arrays.toString(array3));
+        System.out.println("====================================================");
+        MergeSortNoR.mergeSortNoR(array);
+        MergeSortNoR.mergeSortNoR(array2);
+        MergeSortNoR.mergeSortNoR(array3);
+        System.out.println("array数组归并排序(非递归)后结果是：" + Arrays.toString(array));
+        System.out.println("array2数组归并排序(非递归)后结果是：" + Arrays.toString(array2));
+        System.out.println("array3数组归并排序(非递归)后结果是：" + Arrays.toString(array3));
+    }
+}
