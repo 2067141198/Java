642d8d3dd95275230e135c3209bc74365a8487ad
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-09-12 13:18:43 +0800

    589 N叉树的后序遍历（递归方法实现）

---
+++ mouth09/day12/src/NAryTreePostorderTraversal.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class NAryTreePostorderTraversal {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> postorder(Node root) {
+        if(root == null) {
+            return list;
+        }
+        for(Node node : root.children) {
+            postorder(node);
+        }
+        list.add(root.val);
+
+        return list;
+    }
+}
+
