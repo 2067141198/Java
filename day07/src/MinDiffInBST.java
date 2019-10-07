8a70d2f1346f711d74ee1673f60609a6cd58472f
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-10-07 23:28:37 +0800

    783 BST树结点最小距离

--- day07/src/MinDiffInBST.java
+++ day07/src/MinDiffInBST.java
@@ -7,20 +7,20 @@ class TreeNode {
 
 public class MinDiffInBST {
     public static int minDiffInBST(TreeNode root) {
-        if(root == null || root.left == null) {
+        if(root == null) {
             return Integer.MAX_VALUE;
         }
-        if(root.right == null && root.left.left == null){
-            return Math.abs(root.val - root.left.val);
+        if(root.left == null && root.right == null) {
+            return Integer.MAX_VALUE;
         }
-        if(root.left.left == null && root.right.left == null) {
-            return Math.min(Math.abs(root.val - root.left.val), Math.abs(root.val - root.right.val));
+        if(root.left == null) {
+            return Math.min(minDiffInBST(root.right), Math.abs(root.val - root.right.val));
         }
-        if(root.left.left == null && root.right.left != null) {
-            return Math.min(Math.abs(root.val - root.left.val), minDiffInBST(root.right));
+        if(root.right == null) {
+            return Math.min(minDiffInBST(root.left), Math.abs(root.val - root.left.val));
         }
-        int left = Math.min(Math.abs(root.val - root.left.val), minDiffInBST(root.left));
-        int right = Math.min(Math.abs(root.val - root.right.val), minDiffInBST(root.right));
+        int left = Math.min(minDiffInBST(root.left), Math.abs(root.val - root.left.val));
+        int right = Math.min(minDiffInBST(root.right), Math.abs(root.val - root.right.val));
         return Math.min(left, right);
     }
     public static void main(String[] args) {
