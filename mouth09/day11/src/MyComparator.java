6139e33f15c707a0a7c342f43db47b1d0b9f3c83
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-10-12 20:06:41 +0800

    some modification

--- mouth09/day11/src/MyComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-class MyComparator implements Comparator<Integer> {
-    @Override
-    public int compare(Integer o1, Integer o2) {
-        return o1.compareTo(o2);
-    }
-}
-
