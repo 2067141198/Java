538ffe3ccbb5964e8802edb2d036d26f6d8d3b37
2067141198 <48401652+2067141198@users.noreply.github.com>
2020-02-04 19:53:56 +0800

    414.第三大的数

---
+++ AllPracticeCode/LeetCode/src/Arrays/ThirdMaximumNumber.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.TreeSet;
+
+public class ThirdMaximumNumber {
+    public int thirdMax(int[] nums) {
+        TreeSet<Integer> tree = new TreeSet();
+
+        for (int i : nums) {
+            tree.add(i);
+        }
+        if (tree.size() < 3)
+            return tree.last();
+        else {
+            tree.remove(tree.last());
+            tree.remove(tree.last());
+            return tree.last();
+        }
+    }
+}
